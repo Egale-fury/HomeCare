13d9c243cad5e2842f1ab95f281e6a8e4faed36c
dpcfury <15606135595@163.com>
2015-06-18 17:12:05 +0800

    删除了test 的类文件

--- src/main/java/com/eagle/community/exception/ApplicationRuntimeException.java
+++ src/main/java/com/eagle/community/exception/ApplicationRuntimeException.java
@@ -1,8 +1,7 @@
 package com.eagle.community.exception;
 
 public class ApplicationRuntimeException extends BaseWebApplicationException{
-
-	private static final long serialVersionUID = 7072950617568541852L;
+	private static final long serialVersionUID = 1L;
 
 	public ApplicationRuntimeException(String developerMessage) {
 		super(500, "Internal System error", developerMessage);
--- src/main/java/com/eagle/community/exception/UnsupportedMethodException.java
+++ src/main/java/com/eagle/community/exception/UnsupportedMethodException.java
@@ -1,6 +1,8 @@
 package com.eagle.community.exception;
 public class UnsupportedMethodException extends BaseWebApplicationException {
 
+	private static final long serialVersionUID = 1L;
+
 	public UnsupportedMethodException() {
 		super(500, "服务器不支持的业务类型", "该业务正在测试阶段，敬请期待");
 		// TODO Auto-generated constructor stub
